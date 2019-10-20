e393212f80d5e92b4492c8996f2a6a661ec0d29c
Pawel Sasin <pawel.sasin@wolterskluwer.com>
2019-10-20 21:54:33 +0200

    new materials

---
+++ EXAMPLES/S5_EX_7/net.js
@@ -0,0 +1,35 @@
+const https = require('https');
+
+const start_time = new Date();
+
+// get
+
+function goGoogle() {
+
+  https.request('https://google.com', function(res){
+
+    res.on('data', function() {});
+
+    res.on('end', function() {
+	console.log('https - google.com ', new Date() - start_time);
+    });
+
+    res.on('error', function(err) {
+       console.log('Error ', err);
+    });
+
+  }).end();
+
+}
+
+
+goGoogle();
+goGoogle();
+goGoogle();
+goGoogle();
+goGoogle();
+goGoogle();
+goGoogle();
+goGoogle();
+goGoogle();
+goGoogle();
---
+++ EXAMPLES/S5_EX_8/net.js
@@ -0,0 +1,52 @@
+const https = require('https');
+const crypto = require('crypto');
+const fs = require('fs');
+
+
+const start_time = new Date();
+
+function goGoogle() {
+  https.request('https://google.com', function(res){
+
+    res.on('data', function() {});
+
+    res.on('end', function() {
+	console.log('https - google.com ', new Date() - start_time);
+    });
+
+    res.on('error', function(err) {
+       console.log('Error ', err);
+    });
+
+  }).end();
+
+}
+
+function calculateHash() {
+
+
+  crypto.pbkdf2('my_password',
+    'salt',
+    1000000,
+    512,
+    'sha512',
+    () => {
+      console.log('Kodowanie 1:', new Date() - start_time);
+    }
+  );
+}
+
+// #######################################################
+
+goGoogle();
+
+fs.readFile('./package.json', 'utf8', function(){
+    console.log('Wczytanie pliku :', new Date() - start_time);
+});
+
+
+calculateHash();
+calculateHash();
+calculateHash();
+calculateHash();
+
